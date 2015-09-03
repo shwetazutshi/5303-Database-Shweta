58f08d65c2893689f00ef33a4009abccea4235f7
shwetazutshi <szutshi@cs2.mwsu.edu>
2015-09-02 21:36:30 -0500

    final files

--- assignment1/create_table.sql
+++ assignment1/create_table.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 01, 2015 at 09:14 PM
+-- Generation Time: Sep 02, 2015 at 09:30 PM
 -- Server version: 5.6.26
 -- PHP Version: 5.5.9-1ubuntu4.11
 
@@ -43,23 +43,3 @@ CREATE TABLE IF NOT EXISTS `users` (
   `picture` varchar(127) NOT NULL,
   PRIMARY KEY (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`gender`, `title`, `first`, `last`, `street`, `city`, `state`, `zip`, `email`, `username`, `password`, `dob`, `phone`, `picture`) VALUES
-('male', 'mr', 'craig', 'duncan', '6806 w belt line rd', 'toowoomba', 'northern territory', '63775', 'craig.duncan@example.com', 'bigpanda896', 'target', 718729417, '08-1171-8190', 'https://randomuser.me/api/portraits/med/men/86.jpg'),
-('male', 'mr', 'daniel', 'ryan', '4847 white oak dr', 'rockhampton', 'western australia', '16124', 'daniel.ryan@example.com', 'lazyfish641', 'winner1', 1252372521, '06-9762-2515', 'https://randomuser.me/api/portraits/med/men/31.jpg'),
-('female', 'mrs', 'deanna', 'montgomery', '1356 oak lawn ave', 'coffs harbour', 'queensland', '60260', 'deanna.montgomery@example.com', 'purpletiger519', 'aztnm', 1144005484, '09-9152-2266', 'https://randomuser.me/api/portraits/med/women/91.jpg'),
-('male', 'mr', 'donald', 'hayes', '7363 pecan acres ln', 'australian capital territory', 'queensland', '43241', 'donald.hayes@example.com', 'crazyleopard329', 'conrad', 348008488, '07-6267-3320', 'https://randomuser.me/api/portraits/med/men/25.jpg'),
-('female', 'ms', 'edith', 'craig', '9727 dogwood ave', 'hervey bay', 'queensland', '40169', 'edith.craig@example.com', 'brownleopard233', 'these', 1391424801, '05-5941-4167', 'https://randomuser.me/api/portraits/med/women/65.jpg'),
-('male', 'mr', 'george', 'lucas', '6721 first street', 'geelong', 'australian capital territory', '28739', 'george.lucas@example.com', 'beautifulmouse169', 'scania', 660263742, '04-8887-3173', 'https://randomuser.me/api/portraits/med/men/65.jpg'),
-('male', 'mr', 'herman', 'kim', '7308 white oak dr', 'nowra', 'victoria', '31302', 'herman.kim@example.com', 'purpleelephant296', 'oedipus', 385060399, '00-7355-5382', 'https://randomuser.me/api/portraits/med/men/13.jpg'),
-('male', 'mr', 'julio', 'webb', '8480 fincher rd', 'townsville', 'tasmania', '98727', 'julio.webb@example.com', 'blackpeacock544', 'skyhawk', 324873686, '07-8070-5960', 'https://randomuser.me/api/portraits/med/men/89.jpg'),
-('male', 'mr', 'kirk', 'payne', '4637 e pecan st', 'darwin', 'northern territory', '65006', 'kirk.payne@example.com', 'goldenkoala942', 'stinger', 819925017, '07-2230-8792', 'https://randomuser.me/api/portraits/med/men/40.jpg'),
-('female', 'mrs', 'rose', 'mckinney', '5403 eason rd', 'warrnambool', 'queensland', '25324', 'rose.mckinney@example.com', 'lazybear607', 'qazwsx', 1065367840, '02-1238-3194', 'https://randomuser.me/api/portraits/med/women/88.jpg');
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- assignment1/load_db.php
+++ assignment1/load_db.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 /*Name: Shweta Zutshi
 Assignment1: Load the table with 1000 unique users
@@ -68,75 +67,4 @@ for($i=0;$i<sizeof($json_array->results);$i++)
 			echo('['.$db->error.']');
 		}
 	}
-=======
-<?php
-/*Name: Shweta Zutshi
-Assignment1: Load the table with 1000 unique users
-*/
-
-//hiding db info 
-$dbinfo = json_decode(file_get_contents("/home/szutshi/5303-Database-Shweta/db.config.json"));
-
-//connecting to database
-$db = new mysqli($dbinfo->user,$dbinfo->password,$dbinfo->host,$dbinfo->dbname);
-
-if($db->connect_errno > 0){
-    die('Unable to connect to database [' . $db->connect_error . ']');
-}
-
-//getting user from random users
-$json = file_get_contents("http://api.randomuser.me/?results=10");
-#print_r($json);
-
-$json_array = json_decode($json);
-#print_r($json_array->results);
-
-//looping through 1000 users
-for($i=0;$i<sizeof($json_array->results);$i++)
-{
-	$gender = $json_array->results[$i]->user->gender;
-	$title = $json_array->results[$i]->user->name->title;
-	$first = $json_array->results[$i]->user->name->first;
-	$last = $json_array->results[$i]->user->name->last;
-	$street = $json_array->results[$i]->user->location->street;
-	$city = $json_array->results[$i]->user->location->city;
-	$state = $json_array->results[$i]->user->location->state;
-	$zip = $json_array->results[$i]->user->location->zip;
-	$email = $json_array->results[$i]->user->email;
-	$username = $json_array->results[$i]->user->username;
-	$password = $json_array->results[$i]->user->password;
-	$dob = $json_array->results[$i]->user->dob;
-	$phone = $json_array->results[$i]->user->phone;
-	$picture = $json_array->results[$i]->user->picture->medium;
-	
-	//query to check if users are unique, email address is primary key
-	$sql1 = "
-			SELECT *
-			FROM users
-			WHERE email = '{$email}'
-			";
-	#echo $sql1."\n";	
-	//running query to check
-	if(!$result1 = $db->query($sql1))
-	{
-		die('There was an error running the query [' . $db->error . ']');
-	}
-	
-	//if there is no error, it will go to second query
-	if(!$result1->num_rows>0)
-	{
-		//query to insert values into the table
-		$sql2 = "
-				INSERT INTO users
-				VALUES('$gender','$title','$first','$last','$street','$city','$state','$zip','$email','$username','$password','$dob','$phone','$picture')
-				";
-		#echo $sql2."\n";	
-		
-		//running the query
-		if(!$result2 = $db->query($sql2))
-		{
-			echo('['.$db->error.']');
-		}
-	}
->>>>>>> 219296dcf80e5fd069be6e739962aff4b597bb65
 }
