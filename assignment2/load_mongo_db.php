16149a70f4823791dd3302b92af284326568f3b0
shwetazutshi <szutshi@cs2.mwsu.edu>
2015-09-29 21:36:24 -0500

    adding assignment 4

---
+++ assignment2/load_mongo_db.php
@@ -0,0 +1,29 @@
+<?php
+/*Name: Shweta Zutshi
+Assignment4: Load the collection with 1000 users 
+*/
+
+// making connection to MongoDB
+$conn = new MongoClient();
+
+// selecting the database
+$db = $conn->szutshi;
+
+// selecting the collection 
+$collection = $db->random_people;
+
+//getting random people info from random users
+$json = file_get_contents("http://api.randomuser.me/?results=1000");
+
+//getting all results and storing it as an array
+$document = json_decode($json);
+
+//print_r($document->results);
+
+//looping through 1000 users
+for($i=0;$i<sizeof($document->results);$i++)
+{
+	$collection->insert($document->results[$i]);
+}
+
+?>
